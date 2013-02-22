2bfb0dfac0b86d0e92f9939fccc24f35e756738c
Martin Magana <magana.web@gmail.com>
2013-02-21 22:55:26 -0800

    buffer catches xdebug
    using an output buffer to catch any errors, specifically from xdebug. if any output has been made prior to the final return, an error object is returned instead with the buffer.

--- excelTransaction.php
+++ excelTransaction.php
@@ -1,22 +1,28 @@
 <?php
 /* All AJAX calls are made to this page, but handled by the controller. This is the equivelant of a bootstrap page. */
 
+ob_start();                                                                     //allows catching of Xdebug errors
+
 define(  "ROOT_PATH", realpath( dirname(__FILE__) )  );//this is used during development so I can move files around without worrying too much about the path
 
 require_once 'inc\php\controller\controllerExcelTransaction.php';               //controller class
 
-$pageController = new controllerExcelTransaction();                             //create a controller
-
-//make an object with all the information exposed through headers, cookies, session, etc, and pass it to the controller
-
 $requestData = json_decode(  stripcslashes( $_GET['data'] )  );                 //get the request data as a JSON object
 
-echo $pageController->handleRequest($requestData);                              //give the request data to the controller
+$pageController = new controllerExcelTransaction();                             //create a controller
 
-//NOTE: the page headers can be controlled and set as a json object
-        
-//NOTE: if the requestData has a column index set, set this value to the session variable
-//see if there is a session variable that has a column index specified for this file.
-//if it does, then set the JSON object's property to this value
-//STOP! Think about this. Specifically the states, will this really work?
+$output = $pageController->handleRequest($requestData);                         //give the request data to the controller
+
+if(ob_get_length()){                                                            //if compiler tried to send output already, there was an error
+    $output = json_encode(  excelError::createError( ob_get_contents() )  );    //create error message out of the contents
+}
+
+ob_end_clean();                                                                 //stop caching and clear buffer
+
+header('HTTP/1.1 200 OK');
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+header('Content-Type: application/json; charset=utf-8');
+header('Content-Length: ' . strlen($output));
+echo $output;
 ?>
