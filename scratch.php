76ce2c4abf43d5f72172e2998de21a6375868088
Martin Magana <magana.web@gmail.com>
2013-02-22 16:29:09 -0800

    todo notes added

--- scratch.php
+++ scratch.php
@@ -1,25 +1,32 @@
 <html>
-    <head><title>Hello world</title></head>
+    <head>
+        <title>Hello world</title>
+    </head>
     <body>
+        <script type="text/javascript">
+            var mystring = '{"name":"Sherie", "lastname": "mickeymaus"}';
+            var myobject = JSON.parse(mystring);
+            document.write(myobject.name);
+        </script>
 <?php
 //define(  ROOT_PATH, realpath( dirname(__FILE__) )  );
 
-require_once 'inc\php\common.php';
+//require_once 'inc\php\common.php';
     //$timeFixed = str_replace('-', '/', "05-29-08");
     //echo $timeFixed;
     //echo "<br>";
     //echo strtotime($timeFixed); //change dashes to slashes?
-    echo is_date("X");
-    echo "<br>";
+    //echo is_date("X");
+    //echo "<br>";
     
     
-        $timePatternB = "/^[0-9]{1,2}[AM]|[PM]$/";
-        if ( ! preg_match($timePatternB, "1PM") )
-        {
-            echo "no pattern match";
-        }
+        //$timePatternB = "/^[0-9]{1,2}[AM]|[PM]$/";
+        //if ( ! preg_match($timePatternB, "1PM") )
+        //{
+        //    echo "no pattern match";
+        //}
 
-        echo "pattern match";
+        //echo "pattern match";
     
     //if( is_date( "05-29-08" ) ){
     //    echo "this is a date";
