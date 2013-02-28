274cece78332a73c1d373174d1c330ff3a595791
Martin Magana <magana.web@gmail.com>
2013-02-28 14:44:47 -0800

    simple UI update
    simple changes made to the UI that better reflect the state it is in

--- index.php
+++ index.php
@@ -6,35 +6,47 @@
     </head>
     
     <body>
-        <a href="javascript:loadExcelClick();">Load Excel File</a>
-        <br>
-        <a href="javascript:updateExcelClick();">Update Excel File</a>
+        <a id="loadLink" href="javascript:loadExcelClick();">Load Spreadsheet File</a>
+        <a id="updateLink" style="display:none;" href="javascript:updateExcelClick();">Update with my changes</a>
+        <div id="transactionMessage" style="display: none;"></div>
         <div id="results" style="display: none;"></div>
         <!-- SCRIPTS -->
         <?php include('inc/php/jsInclude.php');                                     //includes all the javascript files needed ?>
         <script type="text/javascript">
+            var excelWorkbook = null;                                           //stores the loaded workbook
             var excelFilePath = "userdata/data.csv";                               //for debug only. AJAX file upload should assign this value otherwise.
             //The message displayed while Excel file is being
             var loadingMessage = 'Please wait while your file is being loaded.<br><img src="img/loader32.gif"/>';
             var updatingMessage = 'Please wait while your file is being updated.<br><img src="img/loader32.gif"/>';
             
             function loadExcelClick(){                                              //fires when link is clicked
-                $('#results').html(loadingMessage).show();                           //show the loading message and animation
+                $('#loadLink').fadeOut(100, function(){
+                   $('#transactionMessage').html(loadingMessage).fadeIn();                           //show the loading message and animation 
+                });
                 excelFile.get(excelFilePath);                                       //gets the specified Excel file and returns a JSON string
             }
             
             function updateExcelClick(){
-                $('#results').html(updatingMessage).show();                           //show the loading message and animation
-                excelFile.update(excelFilePath);                                       //gets the specified Excel file and returns a JSON string
+                $('#updateLink').fadeOut(100);
+                $('#results').fadeOut(100, function(){
+                    $('#transactionMessage').html(updatingMessage).fadeIn();                           //show the loading message and animation          
+                });
+                excelFile.update(excelFilePath);                                       //gets the specified Excel file and returns a JSON string 
             }
 
             //displayExcelData function MUST be implemented
             //do whatever you want with the returned JSON object
             function displayExcelData(data){
                 if(data.responseStatus !== "error"){                                 //make sure there are now errors
+                    excelWorkbook = data;
+                    $('#transactionMessage').fadeOut(500, function(){
+                      $('#updateLink').fadeIn();//maybe don't show this until a change has been made
+                      $('#results').fadeIn();
+                    });
+                    
                     var htmlOut = "";
-                    for(var i=0;i<data.excelWorksheets.length;i++){
-                        var excelWorksheet = data.excelWorksheets[i];                                 //the returned Excel file as a JSON object
+                    for(var i=0;i<excelWorkbook.excelWorksheets.length;i++){
+                        var excelWorksheet = excelWorkbook.excelWorksheets[i];                                 //the returned Excel file as a JSON object
                         htmlOut += "<h3 title='Sheet Name'>";
                         htmlOut += excelWorksheet.title;
                         htmlOut += "</h3>";
@@ -60,8 +72,8 @@
                     }
                     $('#results').html(htmlOut);
                 } else {
-                    var errorMessage = excelError.errorType[data.errorType].message;
-                    errorMessage += "<br>" + data.errorMessage;
+                    var errorMessage = excelError.errorType[excelWorkbook.errorType].message;
+                    errorMessage += "<br>" + excelWorkbook.errorMessage;
                     $('#results').html(errorMessage);           //show an error message
                 }
             }
